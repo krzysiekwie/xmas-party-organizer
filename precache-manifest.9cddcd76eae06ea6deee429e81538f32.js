e55dca120094a4c071c960828835bb9317b0955d
Krzysztof Wierzbicki <krzysiekwie@gmail.com>
2019-12-16 13:44:08 +0100

    Updates

---
+++ precache-manifest.9cddcd76eae06ea6deee429e81538f32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaae9f37d730b6de8e7691c23690254",
+    "url": "/xmas-party-organizer/index.html"
+  },
+  {
+    "revision": "f19a6be9e161ffcc5254",
+    "url": "/xmas-party-organizer/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f36b34a7df10b84bd684",
+    "url": "/xmas-party-organizer/static/css/main.6ad8bf76.chunk.css"
+  },
+  {
+    "revision": "f19a6be9e161ffcc5254",
+    "url": "/xmas-party-organizer/static/js/2.87432ae2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/xmas-party-organizer/static/js/2.87432ae2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f36b34a7df10b84bd684",
+    "url": "/xmas-party-organizer/static/js/main.17747c12.chunk.js"
+  },
+  {
+    "revision": "6830bab616dbf2c01b6d",
+    "url": "/xmas-party-organizer/static/js/runtime-main.413f0898.js"
+  },
+  {
+    "revision": "35a0f067e0258b608a4a7a420821ed1d",
+    "url": "/xmas-party-organizer/static/media/PartybyTom.35a0f067.woff"
+  }
+]);
